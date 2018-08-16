hanzoai/s3

commit 99c75fc353ca99f2b8003e82c3bcb673cc297dbc

Extend quorum based listing for prefixes (#5749)
Previous PR 3d58e09191c20ed6bcffc420edfb4975d5031c7f fixed
the issue of quorum based listing for regular objects, this
PR continues on this idea by extending this support to
object directory prefixes as well.

Fixes #5733

cmd/xl-sets.go
@@ -725,27 +725,22 @@ func (s *xlSets) ListObjects(ctx context.Context, bucket, prefix, marker, delimi
 			return result, toObjectErr(walkResult.err, bucket, prefix)
 		}
 
-		entry := walkResult.entry
 		var objInfo ObjectInfo
-		if hasSuffix(entry, slashSeparator) {
-			// Object name needs to be full path.
-			objInfo.Bucket = bucket
-			objInfo.Name = entry
-			objInfo.IsDir = true
+		var err error
+		if hasSuffix(walkResult.entry, slashSeparator) {
+			objInfo, err = s.getHashedSet(walkResult.entry).getObjectInfoDir(bucket, walkResult.entry)
 		} else {
-			// Set the Mode to a "regular" file.
-			var err error
-			objInfo, err = s.getHashedSet(entry).getObjectInfo(bucket, entry)
-			if err != nil {
-				// Ignore errFileNotFound as the object might have got
-				// deleted in the interim period of listing and getObjectInfo(),
-				// ignore quorum error as it might be an entry from an outdated disk.
-				switch errors.Cause(err) {
-				case errFileNotFound, errXLReadQuorum:
-					continue
-				}
-				return result, toObjectErr(err, bucket, prefix)
+			objInfo, err = s.getHashedSet(walkResult.entry).getObjectInfo(bucket, walkResult.entry)
+		}
+		if err != nil {
+			// Ignore errFileNotFound as the object might have got
+			// deleted in the interim period of listing and getObjectInfo(),
+			// ignore quorum error as it might be an entry from an outdated disk.
+			switch errors.Cause(err) {
+			case errFileNotFound, errXLReadQuorum:
+				continue
 			}
+			return result, toObjectErr(err, bucket, prefix)
 		}
 		nextMarker = objInfo.Name
 		objInfos = append(objInfos, objInfo)
@@ -1270,23 +1265,19 @@ func (s *xlSets) listObjectsHeal(bucket, prefix, marker, delimiter string, maxKe
 		if walkResult.err != nil {
 			return loi, toObjectErr(walkResult.err, bucket, prefix)
 		}
-		entry := walkResult.entry
 		var objInfo ObjectInfo
-		if hasSuffix(entry, slashSeparator) {
-			// Object name needs to be full path.
-			objInfo.Bucket = bucket
-			objInfo.Name = entry
-			objInfo.IsDir = true
+		var err error
+		if hasSuffix(walkResult.entry, slashSeparator) {
+			objInfo, err = s.getHashedSet(walkResult.entry).getObjectInfoDir(bucket, walkResult.entry)
 		} else {
-			var err error
-			objInfo, err = s.getHashedSet(entry).getObjectInfo(bucket, entry)
-			if err != nil {
-				// Ignore errFileNotFound
-				if errors.Cause(err) == errFileNotFound {
-					continue
-				}
-				return loi, toObjectErr(err, bucket, prefix)
+			objInfo, err = s.getHashedSet(walkResult.entry).getObjectInfo(bucket, walkResult.entry)
+		}
+		if err != nil {
+			// Ignore errFileNotFound
+			if errors.Cause(err) == errFileNotFound {
+				continue
 			}
+			return loi, toObjectErr(err, bucket, prefix)
 		}
 		nextMarker = objInfo.Name
 		objInfos = append(objInfos, objInfo)

commit 43ca2b6c22aae295c3578db71dbf5496d7869086

Validate prefixes on all sets (#6294)
This PR fixes a regression introduced in 99c75fc353ca99f2b8003e82c3bcb673cc297dbc
where hashing technique was used on prefixes to get the right set
to perform the operation, this is not correct since prefixes and
their corresponding keys might hash to a different value depending
on the key length.

For prefixes/directories we should look everywhere to support proper
quorum based listing.

Fixes #6293

cmd/xl-sets.go
@@ -714,7 +714,15 @@ func (s *xlSets) ListObjects(ctx context.Context, bucket, prefix, marker, delimi
 		}
 
 		isLeafDir := func(bucket, entry string) bool {
-			return s.getHashedSet(entry).isObjectDir(bucket, entry)
+			// Verify prefixes in all sets.
+			var ok bool
+			for _, set := range s.sets {
+				ok = set.isObjectDir(bucket, entry)
+				if ok {
+					return true
+				}
+			}
+			return false
 		}
 
 		var setDisks = make([][]StorageAPI, len(s.sets))
@@ -742,7 +750,13 @@ func (s *xlSets) ListObjects(ctx context.Context, bucket, prefix, marker, delimi
 		var objInfo ObjectInfo
 		var err error
 		if hasSuffix(walkResult.entry, slashSeparator) {
-			objInfo, err = s.getHashedSet(walkResult.entry).getObjectInfoDir(ctx, bucket, walkResult.entry)
+			// Verify prefixes in all sets.
+			for _, set := range s.sets {
+				objInfo, err = set.getObjectInfoDir(ctx, bucket, walkResult.entry)
+				if err == nil {
+					break
+				}
+			}
 		} else {
 			objInfo, err = s.getHashedSet(walkResult.entry).getObjectInfo(ctx, bucket, walkResult.entry)
 		}
@@ -1349,7 +1363,14 @@ func (s *xlSets) listObjectsHeal(ctx context.Context, bucket, prefix, marker, de
 		}
 
 		isLeafDir := func(bucket, entry string) bool {
-			return s.getHashedSet(entry).isObjectDir(bucket, entry)
+			var ok bool
+			for _, set := range s.sets {
+				ok = set.isObjectDir(bucket, entry)
+				if ok {
+					return true
+				}
+			}
+			return false
 		}
 
 		var setDisks = make([][]StorageAPI, len(s.sets))