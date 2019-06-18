hanzoai/s3

commit 03af54b91a9f28daaf3dfba428709c06d5d7770d

Refreshing notification system should not erase the rules-map of other buckets (#7758)
Fixes #7707

cmd/notification.go
@@ -656,7 +656,9 @@ func (sys *NotificationSys) refresh(objAPI ObjectLayer) error {
 		ctx := logger.SetReqInfo(context.Background(), &logger.ReqInfo{BucketName: bucket.Name})
 		config, err := readNotificationConfig(ctx, objAPI, bucket.Name)
 		if err != nil && err != errNoSuchNotifications {
-			return err
+			if _, ok := err.(*event.ErrARNNotFound); ok {
+				continue
+			}
 		}
 		if err == errNoSuchNotifications {
 			continue

commit d1278b7bd927179406bf9d9f21aec77d181f55d2

[notify] Make sure to return when quorum is missing (#7799)
Fixes a regression introduced in 03af54b91a9f28daaf3dfba428709c06d5d7770d

cmd/notification.go
@@ -659,6 +659,7 @@ func (sys *NotificationSys) refresh(objAPI ObjectLayer) error {
 			if _, ok := err.(*event.ErrARNNotFound); ok {
 				continue
 			}
+			return err
 		}
 		if err == errNoSuchNotifications {
 			continue