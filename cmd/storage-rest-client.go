hanzoai/s3

commit 61f9bbe2b061102a27b9be6dcb2d065f26bcdce6

fix: preserve modTime for DeleteMarker on remote disks (#9905)

cmd/storage-rest-client.go
@@ -287,10 +287,12 @@ func (client *storageRESTClient) DeleteVersion(volume, path string, fi FileInfo)
 	values := make(url.Values)
 	values.Set(storageRESTVolume, volume)
 	values.Set(storageRESTFilePath, path)
-	values.Set(storageRESTVersionID, fi.VersionID)
-	values.Set(storageRESTDeleteMarker, strconv.FormatBool(fi.Deleted))
 
-	respBody, err := client.call(storageRESTMethodDeleteVersion, values, nil, -1)
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	encoder.Encode(&fi)
+
+	respBody, err := client.call(storageRESTMethodDeleteVersion, values, &buffer, -1)
 	defer http.DrainBody(respBody)
 	return err
 }

cmd/storage-rest-server.go
@@ -286,10 +286,15 @@ func (s *storageRESTServer) DeleteVersionHandler(w http.ResponseWriter, r *http.
 	vars := mux.Vars(r)
 	volume := vars[storageRESTVolume]
 	filePath := vars[storageRESTFilePath]
-	versionID := vars[storageRESTVersionID]
-	deleteMarker := vars[storageRESTDeleteMarker] == "true"
 
-	err := s.storage.DeleteVersion(volume, filePath, FileInfo{VersionID: versionID, Deleted: deleteMarker})
+	var fi FileInfo
+	decoder := gob.NewDecoder(r.Body)
+	if err := decoder.Decode(&fi); err != nil {
+		s.writeErrorResponse(w, err)
+		return
+	}
+
+	err := s.storage.DeleteVersion(volume, filePath, fi)
 	if err != nil {
 		s.writeErrorResponse(w, err)
 	}

cmd/xl-storage-format-v2.go
@@ -18,6 +18,7 @@ package cmd
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -230,13 +231,17 @@ func (z *xlMetaV2) AddVersion(fi FileInfo) error {
 		if err != nil {
 			return err
 		}
-		z.Versions = append(z.Versions, xlMetaV2Version{
+		v := xlMetaV2Version{
 			Type: DeleteType,
 			DeleteMarker: &xlMetaV2DeleteMarker{
 				VersionID: uv,
 				ModTime:   fi.ModTime.UnixNano(),
 			},
-		})
+		}
+		if !v.Valid() {
+			return errors.New("internal error: invalid version entry generated")
+		}
+		z.Versions = append(z.Versions, v)
 		return nil
 	}
 
@@ -303,6 +308,10 @@ func (z *xlMetaV2) AddVersion(fi FileInfo) error {
 		} else {
 			ventry.ObjectV2.MetaUser[k] = v
 		}
+	}
+
+	if !ventry.Valid() {
+		return errors.New("internal error: invalid version entry generated")
 	}
 
 	for i, version := range z.Versions {

commit f196ab1df38e020034aa8a5d69432ff7f6e36ce3

fix: crash in storage rest client due to spurious query params (#9924)
regression got introduced in 61f9bbe2b061102a27b9be6dcb2d065f26bcdce6
when the DeleteVersion API was changed, but the corresponding query
params were left in-tact.

cmd/storage-rest-client.go
@@ -289,8 +289,9 @@ func (client *storageRESTClient) DeleteVersion(volume, path string, fi FileInfo)
 	values.Set(storageRESTFilePath, path)
 
 	var buffer bytes.Buffer
-	encoder := gob.NewEncoder(&buffer)
-	encoder.Encode(&fi)
+	if err := gob.NewEncoder(&buffer).Encode(fi); err != nil {
+		return err
+	}
 
 	respBody, err := client.call(storageRESTMethodDeleteVersion, values, &buffer, -1)
 	defer http.DrainBody(respBody)

cmd/storage-rest-common.go
@@ -61,7 +61,6 @@ const (
 	storageRESTFilePath      = "file-path"
 	storageRESTVersionID     = "version-id"
 	storageRESTTotalVersions = "total-versions"
-	storageRESTDeleteMarker  = "delete-marker"
 	storageRESTSrcVolume     = "source-volume"
 	storageRESTSrcPath       = "source-path"
 	storageRESTDataDir       = "data-dir"

cmd/storage-rest-server.go
@@ -287,9 +287,13 @@ func (s *storageRESTServer) DeleteVersionHandler(w http.ResponseWriter, r *http.
 	volume := vars[storageRESTVolume]
 	filePath := vars[storageRESTFilePath]
 
+	if r.ContentLength < 0 {
+		s.writeErrorResponse(w, errInvalidArgument)
+		return
+	}
+
 	var fi FileInfo
-	decoder := gob.NewDecoder(r.Body)
-	if err := decoder.Decode(&fi); err != nil {
+	if err := gob.NewDecoder(r.Body).Decode(&fi); err != nil {
 		s.writeErrorResponse(w, err)
 		return
 	}
@@ -300,7 +304,7 @@ func (s *storageRESTServer) DeleteVersionHandler(w http.ResponseWriter, r *http.
 	}
 }
 
-// ReadVersion delete updated metadata.
+// ReadVersion read metadata of versionID
 func (s *storageRESTServer) ReadVersionHandler(w http.ResponseWriter, r *http.Request) {
 	if !s.IsValid(w, r) {
 		return
@@ -858,7 +862,7 @@ func registerStorageRESTHandlers(router *mux.Router, endpointZones EndpointZones
 			subrouter.Methods(http.MethodPost).Path(storageRESTVersionPrefix + storageRESTMethodWriteMetadata).HandlerFunc(httpTraceHdrs(server.WriteMetadataHandler)).
 				Queries(restQueries(storageRESTVolume, storageRESTFilePath)...)
 			subrouter.Methods(http.MethodPost).Path(storageRESTVersionPrefix + storageRESTMethodDeleteVersion).HandlerFunc(httpTraceHdrs(server.DeleteVersionHandler)).
-				Queries(restQueries(storageRESTVolume, storageRESTFilePath, storageRESTVersionID, storageRESTDeleteMarker)...)
+				Queries(restQueries(storageRESTVolume, storageRESTFilePath)...)
 			subrouter.Methods(http.MethodPost).Path(storageRESTVersionPrefix + storageRESTMethodReadVersion).HandlerFunc(httpTraceHdrs(server.ReadVersionHandler)).
 				Queries(restQueries(storageRESTVolume, storageRESTFilePath, storageRESTVersionID)...)
 			subrouter.Methods(http.MethodPost).Path(storageRESTVersionPrefix + storageRESTMethodRenameData).HandlerFunc(httpTraceHdrs(server.RenameDataHandler)).